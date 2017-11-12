895a5a2c7956ddbc9944cb96c6d303b39b30d7d2
kevin <kev.giguere.l@gmail.com>
2017-11-12 17:57:50 -0500

    compose interdependant modules

---
+++ test/composeModule.js
@@ -0,0 +1,95 @@
+(function() {
+  var assert, chai, clearOutput, coffee, concat, gulp, merge, wrapper;
+
+  chai = require('chai');
+
+  chai.use(require('chai-fs'));
+
+  assert = chai.assert;
+
+  wrapper = require('../lib/spark-wrapper');
+
+  clearOutput = require('./clearOutput');
+
+  gulp = require('gulp');
+
+  merge = require('merge2');
+
+  coffee = require('gulp-coffee');
+
+  concat = require('gulp-concat');
+
+  describe('Compose with modules', function() {
+    beforeEach(clearOutput);
+    it('compose external module', function(done) {
+      return merge([
+        gulp.src(['./test/files/_remove_require.coffee']), wrapper.composeModule({
+          namespace: 'Spark.MyModule',
+          module: 'my-module'
+        }, '*.coffee'), gulp.src(['./test/files/ExternalDependantClass.coffee'])
+      ]).pipe(wrapper.compose({
+        namespace: 'Spark'
+      })).pipe(concat('spark.coffee')).pipe(coffee()).pipe(gulp.dest('./test/output/')).on('end', function() {
+        var Spark, obj;
+        assert.pathExists('./test/output/spark.js');
+        Spark = require('./output/spark.js');
+        assert.isDefined(Spark.MyModule);
+        assert.isFunction(Spark.MyModule.CompiledClass);
+        assert.isFunction(Spark.MyModule.CompiledClass.definition);
+        obj = new Spark.MyModule.CompiledClass();
+        assert.equal(obj.hello(), 'hello', 'CompiledClass::hello');
+        obj = new Spark.ExternalDependantClass();
+        assert.equal(obj.hello(), 'hello', 'ExternalDependantClass::hello');
+        return done();
+      });
+    });
+    it('compose external module same namespace', function(done) {
+      return merge([
+        gulp.src(['./test/files/_remove_require.coffee']), wrapper.composeModule({
+          namespace: 'Spark',
+          module: 'my-module'
+        }, '*.coffee'), gulp.src(['./test/files/ExternalDependantClass.coffee'])
+      ]).pipe(wrapper.compose({
+        namespace: 'Spark'
+      })).pipe(concat('spark.coffee')).pipe(coffee()).pipe(gulp.dest('./test/output/')).on('end', function() {
+        var Spark, obj;
+        assert.pathExists('./test/output/spark.js');
+        Spark = require('./output/spark.js');
+        assert.isFunction(Spark.CompiledClass);
+        assert.isFunction(Spark.CompiledClass.definition);
+        obj = new Spark.CompiledClass();
+        assert.equal(obj.hello(), 'hello', 'CompiledClass::hello');
+        obj = new Spark.ExternalDependantClass();
+        assert.equal(obj.hello(), 'hello', 'ExternalDependantClass::hello');
+        return done();
+      });
+    });
+    return it('compose interdependant modules', function(done) {
+      return merge([
+        gulp.src(['./test/files/_remove_require.coffee']), wrapper.composeModule({
+          namespace: 'Spark',
+          module: 'my-module'
+        }, '*.coffee').pipe(wrapper.composeModule({
+          namespace: 'Spark',
+          module: 'my-module2'
+        }, '*.coffee')), gulp.src(['./test/files/ExternalDependantClass.coffee'])
+      ]).pipe(wrapper.compose({
+        namespace: 'Spark'
+      })).pipe(concat('spark.coffee')).pipe(coffee()).pipe(gulp.dest('./test/output/')).on('end', function() {
+        var Spark, obj;
+        assert.pathExists('./test/output/spark.js');
+        Spark = require('./output/spark.js');
+        assert.isFunction(Spark.CompiledClass);
+        assert.isFunction(Spark.CompiledClass.definition);
+        obj = new Spark.CompiledClass();
+        assert.equal(obj.hello(), 'hello', 'CompiledClass::hello');
+        obj = new Spark.ExternalDependantClass();
+        assert.equal(obj.hello(), 'hello', 'ExternalDependantClass::hello');
+        obj = new Spark.InterDependantClass();
+        assert.equal(obj.hello(), 'hello', 'InterDependantClass::hello');
+        return done();
+      });
+    });
+  });
+
+}).call(this);
--- test/src/compose.coffee
+++ test/src/compose.coffee
@@ -13,7 +13,12 @@ describe 'Compose', ->
 
   it 'compose and concat files', (done)->
     assert.notPathExists('./test/output/spark.js')
-    gulp.src(['./test/files/DependantClass.coffee','./test/files/DependantCommentClass.coffee','./test/files/CompiledClass.coffee'])
+    gulp.src([
+      './test/files/_remove_require.coffee'
+      './test/files/DependantClass.coffee'
+      './test/files/DependantCommentClass.coffee'
+      './test/files/CompiledClass.coffee'
+    ])
       .pipe(wrapper.compose({namespace:'Spark'}))
       .pipe(concat('spark.coffee'))
       .pipe(coffee())
@@ -33,6 +38,7 @@ describe 'Compose', ->
   it 'compose with multiple call', (done)->
     assert.notPathExists('./test/output/spark.js')
     merge([
+      gulp.src(['./test/files/_remove_require.coffee'])
       gulp.src(['./test/files/DependantCommentClass.coffee','./test/files/CompiledClass.coffee'])
         .pipe(wrapper.compose({namespace:'Spark'}))
       gulp.src(['./test/files/IndirectDependantClass.coffee'])
@@ -51,44 +57,3 @@ describe 'Compose', ->
       obj = new Spark.IndirectDependantClass()
       assert.equal obj.hello(), 'hello', 'IndirectDependantClass::hello'
       done()
-
-  it 'compose external module', (done)->
-      merge([
-          wrapper.composeModule({namespace:'Spark.MyModule',module:'my-module'},'*.coffee')
-          gulp.src(['./test/files/ExternalDependantClass.coffee'])
-      ])
-      .pipe(wrapper.compose({namespace:'Spark'}))
-      .pipe(concat('spark.coffee'))
-      .pipe(coffee())
-      .pipe(gulp.dest('./test/output/'))
-      .on 'end', ->
-        assert.pathExists('./test/output/spark.js')
-        Spark = require('./output/spark.js')
-        assert.isDefined(Spark.MyModule)
-        assert.isFunction(Spark.MyModule.CompiledClass)
-        assert.isFunction(Spark.MyModule.CompiledClass.definition)
-        obj = new Spark.MyModule.CompiledClass()
-        assert.equal obj.hello(), 'hello', 'CompiledClass::hello'
-        obj = new Spark.ExternalDependantClass()
-        assert.equal obj.hello(), 'hello', 'ExternalDependantClass::hello'
-        done()
-
-  it 'compose external module same namespace', (done)->
-      merge([
-          wrapper.composeModule({namespace:'Spark',module:'my-module'},'*.coffee')
-          gulp.src(['./test/files/ExternalDependantClass.coffee'])
-      ])
-      .pipe(wrapper.compose({namespace:'Spark'}))
-      .pipe(concat('spark.coffee'))
-      .pipe(coffee())
-      .pipe(gulp.dest('./test/output/'))
-      .on 'end', ->
-        assert.pathExists('./test/output/spark.js')
-        Spark = require('./output/spark.js')
-        assert.isFunction(Spark.CompiledClass)
-        assert.isFunction(Spark.CompiledClass.definition)
-        obj = new Spark.CompiledClass()
-        assert.equal obj.hello(), 'hello', 'CompiledClass::hello'
-        obj = new Spark.ExternalDependantClass()
-        assert.equal obj.hello(), 'hello', 'ExternalDependantClass::hello'
-        done()
